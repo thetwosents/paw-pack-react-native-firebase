fdd400d6142b799dc613fd0c7566eae9a9b8d8e7
Jon Senterfitt <thetwosents@gmail.com>
2022-02-16 11:32:33 -0600

    Added add dog functino

--- screens/AddDogScreen.js
+++ screens/AddDogScreen.js
@@ -1,110 +1,127 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { Formik } from 'formik';
+import { Text, StyleSheet, Button } from 'react-native';
+import { Formik, Field } from 'formik';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { View, TextInput, Logo, Button, FormErrorMessage } from '../components';
+import { View, TextInput, Logo, FormErrorMessage } from '../components';
 import { Images, Colors, auth } from '../config';
+import { getDatabase, ref, set } from "firebase/database";
 
 export const AddDogScreen = () => {
+  const handleSubmit = (values) => {
+    // Formik values are available here
+    const userId = auth.currentUser.uid;
+    const dogRef = ref.child(`users/${userId}/dogs`);
+    // Create a unique id for the dog
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId + '/dogs/'), {
+      name: values.name,
+      breed: values.breed
+    });
 
-    return (
-        <>
-            <View isSafe style={styles.container}>
-                <KeyboardAwareScrollView enableOnAndroid={true}>
-                    <Formik
-                        initialValues={{
-                            name: '',
-                            breed: '',
-                        }}
-                        onSubmit={values => console.log(values)}
-                    >
-                        {({
-                            values,
-                            touched,
-                            errors,
-                            handleChange,
-                        }) => (
-                            <>
-                                {/* Input fields */}
-                                <TextInput
-                                    name='name'
-                                    leftIconName='email'
-                                    placeholder='Enter name'
-                                    autoCapitalize='none'
-                                    keyboardType='name'
-                                    textContentType='name'
-                                    autoFocus={true}
-                                    value={values.name}
-                                    onChangeText={handleChange('name')}
-                                />
-                                <TextInput
-                                    name='breed'
-                                    leftIconName='email'
-                                    placeholder='Enter breed'
-                                    autoCapitalize='none'
-                                    keyboardType='name'
-                                    textContentType='name'
-                                    autoFocus={true}
-                                    value={values.breed}
-                                    onChangeText={handleChange('breed')}
-                                />
-                                <Button
-                                    title='Add dog'
-                                    onPress={() => console.log(values)}
-                                />
-                            </>
-                        )}
-                    </Formik>
-                </KeyboardAwareScrollView>
-            </View>
-        </>
-    );
+    // Navigate to HomeScreen
+    navigation.navigate('Home');
+    // Then navigate to /dogs screen
+  };
+  return (
+    <>
+      <View isSafe style={styles.container}>
+        <KeyboardAwareScrollView enableOnAndroid={true}>
+          <Formik
+            initialValues={{
+              name: '',
+              breed: '',
+            }}
+
+            onSubmit={values => handleSubmit(values)}
+            id="formik"
+          >
+            {({
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleSubmit
+            }) => (
+              <>
+                {/* Input fields */}
+                <TextInput
+                  name='name'
+                  leftIconName='email'
+                  placeholder='Enter name'
+                  autoCapitalize='none'
+                  keyboardType='default'
+                  textContentType='name'
+                  autoFocus={true}
+                  value={values.name}
+                  onChangeText={handleChange('name')}
+                />
+                <TextInput
+                  name='breed'
+                  leftIconName='email'
+                  placeholder='Enter breed'
+                  autoCapitalize='none'
+                  keyboardType='default'
+                  textContentType='name'
+                  autoFocus={true}
+                  value={values.breed}
+                  onChangeText={handleChange('breed')}
+                />
+                <Button
+                  title='Add dog'
+                  onPress={handleSubmit}
+                />
+              </>
+            )}
+          </Formik>
+        </KeyboardAwareScrollView>
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.white,
-      paddingHorizontal: 12
-    },
-    logoContainer: {
-      alignItems: 'center'
-    },
-    screenTitle: {
-      fontSize: 32,
-      fontWeight: '700',
-      color: Colors.black,
-      paddingTop: 20
-    },
-    footer: {
-      backgroundColor: Colors.white,
-      paddingHorizontal: 12,
-      paddingBottom: 48,
-      alignItems: 'center'
-    },
-    footerText: {
-      fontSize: 14,
-      fontWeight: '700',
-      color: Colors.orange
-    },
-    button: {
-      width: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 8,
-      backgroundColor: Colors.orange,
-      padding: 10,
-      borderRadius: 8
-    },
-    buttonText: {
-      fontSize: 20,
-      color: Colors.white,
-      fontWeight: '700'
-    },
-    borderlessButtonContainer: {
-      marginTop: 16,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    paddingHorizontal: 12
+  },
+  logoContainer: {
+    alignItems: 'center'
+  },
+  screenTitle: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: Colors.black,
+    paddingTop: 20
+  },
+  footer: {
+    backgroundColor: Colors.white,
+    paddingHorizontal: 12,
+    paddingBottom: 48,
+    alignItems: 'center'
+  },
+  footerText: {
+    fontSize: 14,
+    fontWeight: '700',
+    color: Colors.orange
+  },
+  button: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8,
+    backgroundColor: Colors.orange,
+    padding: 10,
+    borderRadius: 8
+  },
+  buttonText: {
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: '700'
+  },
+  borderlessButtonContainer: {
+    marginTop: 16,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,41 +8,18 @@ export const HomeScreen = ({ navigation }) => {
   const handleLogout = () => {
     signOut(auth).catch(error => console.log('Error logging out: ', error));
   };
-  // Handle navigation
   const handleNavigation = (route) => {
     navigation.navigate(route);
   };
   return (
     <View style={styles.container}>
       {/* A button to add a dog to your account and navigate to /add-dog screen */}
-      <Button
-        title="Add a dog"
-        // Navigate to AddDog screen
-        onPress={() => handleNavigation('AddDog')}
-        color="#841584"
-      />
-
-    <Button
-        title="Make a connection"
-        onPress={() => console.log('Make a connection')}
-        color="#841584"
-      />
-
-      <Button
-        title="Add your park"
-        onPress={() => console.log('Add your park')}
-        color="#841584"
-      />
-
-      {/* Make a group or community called a pack and let members invite other users to their "Pack" */}
-      <Button
-        title="Make a pack"
-        onPress={() => console.log('Make a pack')}
-        color="#841584"
-      />
+      <Button title="Add a dog" onPress={() => handleNavigation('AddDog')} color="#841584" />
+      <Button title="Make a connection" onPress={() => console.log('Make a connection')} color="#841584" />
+      <Button title="Add your park" onPress={() => console.log('Add your park')} />
+      <Button title="Make a pack" onPress={() => console.log('Make a pack')} />
       <Button title="Check in" onPress={() => console.log('Check in')} />
       <Button title="Invite to pack" onPress={() => console.log('Invite to pack')} color="#841584" />
-
       <Button title='Sign Out' onPress={handleLogout} />
     </View>
   );
